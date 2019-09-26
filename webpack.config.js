94a57ea19549b5a61966f9408dd5a179004b8943
liuxiang <350646436@qq.com>
2019-09-26 14:01:07 +0800

    add devOps conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const HTML = require('html-webpack-plugin')
 module.exports = {
   mode: 'production',
   entry: { app: './src/app.js' },
   output: {
     path: __dirname + '/build',
     filename: '[name].js'
-  }
+  },
+  plugins: [new HTML()]
 }
